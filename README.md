2baf7040009417ae12634bd838094bcf42b9503a
Joshua Cadavez <jcadavez@insider.com>
2020-10-22 01:39:29 -0400

    Refactor utils

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Compares Articles JSON Files
 1. Download local branch
 2. Add your JSON object from each article after the `module.exports = `
     - _article-prod.js_ is for Production articles
-    - _article-bi-story.js_ is for Dev articles
+    - _article-dev.js_ is for Dev articles
 3. Run the test by running this project by command `npm run test`
 4. Each * is a layer when comparing the JSON object
 5. These are the statuses
--- modules/utils.js
+++ modules/utils.js
@@ -1,38 +1,33 @@
 let utils = {
     compare: function(o, n, layer=1, failures=0) {
         if(layer == 1) console.log(`\t\t\t\t\t\t\tPROD\tvs\tDEV`);
-        for(const [key, value] of Object.entries(o)){
+        for(const [key, value] of Object.entries(o)) {
             let type = typeof value;
-            if(type === 'object' && type !== null){
-                console.log(`\t${"*".repeat(layer)} ${key}`);
-                if(typeof n[key] !== 'undefined'){
-                    failures = failures + this.compare(o[key], n[key], layer+1);
-                    delete n[key];
-                } else { 
-                    failures++;
-                    console.log(`\tFAIL |\t\t${key}\t${'\t'.repeat(4 - key.length/7)}${JSON.stringify(o[key])}\tvs\tN/A`);
-                }
-            }
-            else if(typeof n[key] === 'undefined'){
+            let rep = 4 - key.length / 7;
+            let target = type === 'object' ? JSON.stringify(value) : value;
+            if(typeof n[key] === 'undefined'){
                 failures++;
-                console.log(`\tFAIL |\t\t${key}\t${'\t'.repeat(4 - key.length/7)}${o[key]}\tvs\tN/A`);
-                delete n[key];
-            }
-            else if(value !== n[key]){
-                failures++;
-                console.log(`\tFAIL |\t\t${key}\t${'\t'.repeat(4 - key.length/7)}${o[key]}\tvs\t${n[key]}`);
-                delete n[key];
+                console.log(`\tFAIL |\t\t${key}\t${'\t'.repeat(rep)}${target}\tvs\tN/A`); 
             } else {
-                console.log(`\tPASS |\t\t${key}\t${'\t'.repeat(4 - key.length/7)}${o[key]}`);
+                if(type === 'object' && type !== null){
+                    console.log(`\t${"*".repeat(layer)} ${key}`);
+                    failures = failures + this.compare(o[key], n[key], layer+1);
+                } else if(value !== n[key]){
+                    failures++;
+                    console.log(`\tFAIL |\t\t${key}\t${'\t'.repeat(rep)}${target}\tvs\t${n[key]}`);
+                } else {
+                    console.log(`\tPASS |\t\t${key}\t${'\t'.repeat(rep)}${target}`);
+                }
                 delete n[key];
             }
         }
         if(JSON.stringify(n) !== JSON.stringify({})) {
+            failures = failures + Object.keys(n).length;
             for(const [key, value] of Object.entries(n)){
-                if(typeof value === 'object') console.log(`\tMISS |\t\t${key}\t${'\t'.repeat(4 - key.length/7)}${JSON.stringify(value)}`);
-                else console.log(`\tMISS |\t\t${key}\t${'\t'.repeat(4 - key.length/7)}${value}`);
+                let flag = typeof value === 'object';
+                let rep = 4 - key.length / 7;
+                console.log(`\tMISS |\t\t${key}\t${'\t'.repeat(rep)}${flag ? JSON.stringify(value) : value}`);
             }
-            failures++;
         }
         return failures;
     }
